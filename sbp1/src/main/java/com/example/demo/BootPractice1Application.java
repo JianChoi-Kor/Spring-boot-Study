aba186e7546b82c58326f66caa5d6dca549c4b41
JianChoi-Kor <wldks79@gmail.com>
2021-04-05 00:36:07 +0900

    basic structure

---
+++ sbp1/src/main/java/com/example/demo/BootPractice1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPractice1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(BootPractice1Application.class, args);
+	}
+
+
+
+}
